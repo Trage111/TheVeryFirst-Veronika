045957be809bdf47fda4ad485736479f70a688d7
Trage111 <31367406+Trage111@users.noreply.github.com>
2017-08-27 19:06:48 +0400

    Update 1.Inversion.cpp

--- 1.Inversion.cpp
+++ 1.Inversion.cpp
@@ -8,9 +8,9 @@ int * invert(int * array, int size){
 	
 	for (int i = 0, j = size - 1; i < size / 2; i++, j--){
 	
-		array[i]+=array[j];
-		array[j]=array[i]-array[j];
-		array[i]-=array[j];
+		array[i] += array[j];
+		array[j] = array[i] - array[j];
+		array[i] -= array[j];
 	}
 	
 	return array;
