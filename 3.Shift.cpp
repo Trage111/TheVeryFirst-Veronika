eec9083f05d4380a3517b957e2210ebf9db6b2b2
Trage111 <31367406+Trage111@users.noreply.github.com>
2017-09-02 14:05:02 +0400

    Update 3.Shift.cpp

--- 3.Shift.cpp
+++ 3.Shift.cpp
@@ -14,22 +14,22 @@ long int * shift (long int * array, long int size, long int offset) {
 	
 	if (offset > 0) {
 		
-		int j = 0;
+		long int j = 0;
 		
-		for (int i = offset - 1; i < size; i++, j++) {
+		for (long int i = offset - 1; i < size; i++, j++) {
 			arrayToReturn[j] = array[i];
 		}
-		for (int i = 0; i < offset - 1; i ++, j++) {
+		for (long int i = 0; i < offset - 1; i ++, j++) {
 			arrayToReturn[j] = array[i];
 		}
 	}
 	else {
-		int j = 0;
+		long int j = 0;
 		
-		for (int i = size - 1 + offset + 1; i < size; i++, j++) {
+		for (long int i = size - 1 + offset + 1; i < size; i++, j++) {
 			arrayToReturn[j] = array[i];
 		}
-		for (int i = 0; i < size - 1 + offset + 1; i++, j++) {
+		for (long int i = 0; i < size - 1 + offset + 1; i++, j++) {
 			arrayToReturn[j] = array[i];
 		}
 	}
@@ -43,14 +43,14 @@ int main() {
 	long int N, K;
 	cin >> N;
 	long int * array = new long int [N];
-	for (int i = 0; i < N; i++) {
+	for (long int i = 0; i < N; i++) {
 		cin >> array[i];
 	}
 	cin >> K;
 	
 	array = shift(array, N, K);
 	
-	for (int i = 0; i < N; i++) {
+	for (long int i = 0; i < N; i++) {
 		cout << array[i] << " ";
 	}
 	
