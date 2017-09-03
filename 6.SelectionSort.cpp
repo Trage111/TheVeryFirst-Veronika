9e5f5e7c7254c1a5ea3b9db40774e63b6bdb0828
Trage111 <31367406+Trage111@users.noreply.github.com>
2017-09-03 22:28:25 +0400

    Add files via upload

---
+++ 6.SelectionSort.cpp
@@ -0,0 +1,43 @@
+// spent: about 15 min
+// result: OK
+
+#include <iostream>
+using namespace std;
+
+void swap (int * x, int * y) {
+	*y += *x;
+	*x = *y - *x;
+	*y -= *x;
+}
+
+void selectionSort (int * array, int size) {
+	
+	int minNum = 0;
+	
+	for (int i = 0; i < size; i++) {
+		for (int j = i + 1; j < size; j++) {
+			if (array[j] < array[minNum]) {
+				minNum = j;
+			}
+		}
+		swap(array[i], array[minNum]);
+		minNum = i + 1;
+	}
+} 
+
+int main() {
+	
+	int s;
+	cin >> s;
+	int * array = new int [s];
+	for (int i = 0; i < s; i++) {
+		cin >> array[i];
+	}
+	selectionSort (array, s);
+	
+	for (int i = 0; i < s; i++) {
+		cout << array[i]; i < s - 1 ? cout << " " : cout << "\n";
+	}
+	
+	return 0;
+}
